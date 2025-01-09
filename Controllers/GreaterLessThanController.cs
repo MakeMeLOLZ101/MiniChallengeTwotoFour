941b5b2325469dcb448a5cb1babd8456c9192b8a
MakeMeLOLZ101 <ianchesley@yahoo.com>
2025-01-09 15:20:24 -0800

    Should be done

--- Controllers/GreaterLessThanController.cs
+++ Controllers/GreaterLessThanController.cs
@@ -8,9 +8,21 @@ using MiniChallengeTwotoFour.Services;
 namespace MiniChallengeTwotoFour.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class GreaterLessThanController : ControllerBase
     {
-        
+        private readonly GreaterLessThanServices _greaterLessThanServices;
+
+        public GreaterLessThanController(GreaterLessThanServices greaterLessThanservices)
+        {
+            _greaterLessThanServices = greaterLessThanservices;
+        }
+
+        [HttpGet]
+        [Route("compare/{firstNum}/{secondNum}")]
+        public string CompareNums(double firstNum, double secondNum)
+        {
+            return _greaterLessThanServices.CompareNums(firstNum, secondNum);
+        }
     }
 }
--- Services/GreaterLessThanServices.cs
+++ Services/GreaterLessThanServices.cs
@@ -7,6 +7,20 @@ namespace MiniChallengeTwotoFour.Services
 {
     public class GreaterLessThanServices
     {
-        
+        public string CompareNums(double firstNum, double secondNum)
+        {
+            if(firstNum > secondNum)
+            {
+                return $"{firstNum} is greater than {secondNum}";
+            }
+            else if(firstNum < secondNum)
+            {
+                return $"{firstNum} is less than {secondNum}";
+            }
+            else
+            {
+                return $"{firstNum} is equal to {secondNum}";
+            }
+        }
     }
 }
